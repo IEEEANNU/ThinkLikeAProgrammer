f3de88d961df7963a34ffda2c21f8f6d60b88cfc
MahmoodKhalid <mahmood.k.samaha@hotmail.com>
2015-09-01 16:45:53 +0300

    initial

---
+++ database/migrations/2015_09_01_131325_create_submissions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSubmissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('submissions', function (Blueprint $table) {
+            $table->increments('id');//SubID
+            $table->integer('user_id')->unsigned();
+            $table->integer('question_id')->unsigned();
+            $table->string('link');
+            $table->boolean('hint_used');
+            $table->boolean('assessed');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('submissions');
+    }
+}
