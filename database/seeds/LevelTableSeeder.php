f782a67d04e4879c593116d19be8cb1de343b8dd
zalsader <zalsader@hotmail.com>
2015-09-10 23:56:42 +0300

    Created seeders for questions, levels, and superadmin user

---
+++ database/seeds/LevelTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use App\Level;
+
+class LevelTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('levels')->delete();
+
+        Level::insert([
+            ['name' => 'Level 1', 'mark' => 5.0],
+            ['name' => 'Level 2', 'mark' => 10.0],
+            ['name' => 'Level 3', 'mark' => 15.0],
+            ['name' => 'Level 4', 'mark' => 20.0],
+            ['name' => 'Level 5', 'mark' => 25.0],
+            ['name' => 'Bonus Level', 'mark' => 30.0],
+        ]);
+    }
+
+}
