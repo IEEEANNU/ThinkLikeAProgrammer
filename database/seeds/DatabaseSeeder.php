09c6a28396d970767345ed25c34911855171a51d
zalsader <zalsader@hotmail.com>
2015-09-10 23:56:42 +0300

    Created seeders for questions, levels, and superadmin user

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,9 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-        // $this->call(UserTableSeeder::class);
+        $this->call(UserTableSeeder::class);
+        $this->call(LevelTableSeeder::class);
+        $this->call(QuestionTableSeeder::class);
 
         Model::reguard();
     }
