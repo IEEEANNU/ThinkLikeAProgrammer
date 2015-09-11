5994a2a343c5cab176732cb46d5b5bb6d06390f1
zalsader <zalsader@hotmail.com>
2015-09-11 19:09:14 +0300

    Questions Images Edited migrations

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -8,7 +8,7 @@ class UserTableSeeder extends Seeder {
 
     public function run()
     {
-        DB::table('users')->delete();
+        DB::table('users')->truncate();
 
         User::create([
             'name' => 'Admin',
